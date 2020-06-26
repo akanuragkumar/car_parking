29c71ef1638091f0b7c803268023c2c5cdfed169
akanuragkumar712 <akanuragkumar712@gmail.com>
2020-06-27 00:37:52 +0530

    removed dict to class objects

--- car_parking/park/views.py
+++ car_parking/park/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 import json
 
-ranger = 0
-f = open('./.env')
-for line in f.readlines():
-    if line:
-        exec(line)
+ranger = 10
 
-car_to_slot = {}
-slots = {str(value): None for value in range(1, ranger + 1)}
+class ParkingArea:
+
+    def __init__(self, slot_number):
+        self.slot_number = slot_number
+        self.car_number = None
+
+    def __str__(self):
+        return str({'slot': self.slot_number, 'car_number': self.car_number})
+
+
+class Parking:
+
+    def __init__(self, max_slots):
+        self.slots = {str(value): ParkingArea(str(value)) for value in range(1, max_slots + 1)}
+        self.car_to_slot = {}
+
+
+def view_parking():
+    print("car_to_slot")
+    for i in parking.slots.values():
+        print(i)
+    print("car_to_slot")
+    for i in parking.car_to_slot.values():
+        print(i)
+
+
+parking = Parking(ranger)
 
 
 def add_car(request):
@@ -20,15 +40,19 @@ where it is parked."""
         car_number = payload.get('car_number')
         if car_number is None:
             return JsonResponse({'Error': 'Wrong Key!'}, status=400)
-        for key, value in slots.items():
-            if value is None:
+        for key, value in parking.slots.items():
+            if value.car_number is None:
                 slot = key
-                slots[key] = car_number
-                car_to_slot[car_number] = key
+                value.car_number = car_number
+                parking.car_to_slot[car_number] = value
                 break
         else:
-            return JsonResponse({'Error': 'Parking full!', 'car_to_slot': car_to_slot, 'slots': slots})
-        return JsonResponse({'slot': slot, 'car_number': car_number, 'car_to_slot': car_to_slot, 'slots': slots})
+
+            return JsonResponse({'Error': 'Parking full!', 'car_to_slot': parking.car_to_slot, 'slots': parking.slots})
+        view_parking()
+        a = {i: str(j) for i, j in parking.car_to_slot.items()}
+        b = {i: str(j) for i, j in parking.slots.items()}
+        return JsonResponse({'slot': slot, 'car_number': car_number, 'car_to_slot': a, 'slots': b})
 
 
 def un_park(request):
@@ -37,15 +61,19 @@ from and frees that slot up."""
     if request.method == 'POST':
         payload = json.loads(request.body)
         slot = payload.get('slot')
-        if slot in slots and slots[slot]:
-            slots[slot] = None
+        if slot in parking.slots and parking.slots[slot].car_number:
+            parking.slots[slot].car_number = None
         else:
-            return JsonResponse({'Error': 'Slot invalid/Empty!', 'car_to_slot': car_to_slot, 'slots': slots})
-        for key, value in car_to_slot.items():
-            if value == slot:
-                del car_to_slot[key]
+            a = {i: str(j) for i, j in parking.car_to_slot.items()}
+            b = {i: str(j) for i, j in parking.slots.items()}
+            return JsonResponse({'Error': 'Slot invalid/Empty!', 'car_to_slot': a, 'slots': b})
+        for key, value in parking.car_to_slot.items():
+            if value.slot_number == slot:
+                del parking.car_to_slot[key]
                 break
-        return JsonResponse({'Success': 'Car removed!', 'car_to_slot': car_to_slot, 'slots': slots})
+        a = {i: str(j) for i, j in parking.car_to_slot.items()}
+        b = {i: str(j) for i, j in parking.slots.items()}
+        return JsonResponse({'Success': 'Car removed!', 'car_to_slot': a, 'slots': b})
 
 
 def get_car(request):
@@ -53,20 +81,25 @@ def get_car(request):
 number and return both the car number and slot number."""
     if request.GET.get('car_number'):
         car_number = request.GET.get('car_number')
-        if car_number not in car_to_slot:
-            return JsonResponse({'Error': 'Car could not be found!','car_to_slot': car_to_slot, 'slots': slots})
+        if car_number not in parking.car_to_slot:
+            a = {i: str(j) for i, j in parking.car_to_slot.items()}
+            b = {i: str(j) for i, j in parking.slots.items()}
+            return JsonResponse({'Error': 'Car could not be found!', 'car_to_slot': a, 'slots': b})
         else:
-            car_slot = car_to_slot[car_number]
+            car_slot = parking.car_to_slot[car_number].slot_number
             return JsonResponse({'slot': car_slot, 'car_number': car_number})
     elif request.GET.get('slot'):
         slot = request.GET.get('slot')
-        if slot not in slots:
-            return JsonResponse({'Error': 'Slot invalid/Empty!', 'car_to_slot': car_to_slot, 'slots': slots})
+        if slot not in parking.slots:
+            a = {i: str(j) for i, j in parking.car_to_slot.items()}
+            b = {i: str(j) for i, j in parking.slots.items()}
+            return JsonResponse({'Error': 'Slot invalid/Empty!', 'car_to_slot': a, 'slots': b})
         else:
-            car_number = slots[slot]
-            return JsonResponse({'slot': slot, 'car_number': car_number, 'car_to_slot': car_to_slot, 'slots': slots})
+            car_number = parking.slots[slot].car_number
+            a = {i: str(j) for i, j in parking.car_to_slot.items()}
+            b = {i: str(j) for i, j in parking.slots.items()}
+            return JsonResponse({'slot': slot, 'car_number': car_number, 'car_to_slot': a, 'slots': b})
     else:
         return JsonResponse({'Error': 'Missing or wrong parameter!'}, status=400)
-
 
 
